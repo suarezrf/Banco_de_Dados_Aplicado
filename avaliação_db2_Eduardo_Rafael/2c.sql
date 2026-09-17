9bfd4317a5d7b4f4fa7e1082cb218fe8a55db152
Eduardo Rafael da Silva Suarez <eduardo.suarez@amf.edu.br>
2026-09-17 20:14:28 -0300

    Create 2c.sql

---
+++ avaliação_db2_Eduardo_Rafael/2c.sql
@@ -0,0 +1,9 @@
+
+select c.id,c.nome, c.email, c.cidade
+from clientes c
+where c.ativo = true
+  and not exists (
+      select 1
+      from emails_bloqueados eb
+      where eb.email = c.email
+  );
