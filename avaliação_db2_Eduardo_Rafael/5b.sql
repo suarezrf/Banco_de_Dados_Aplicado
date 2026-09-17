ee00e6eeb97bb5d2c9753e115f3c7454859c7ea6
Eduardo Rafael da Silva Suarez <eduardo.suarez@amf.edu.br>
2026-09-17 20:20:29 -0300

    Create 5b.sql

---
+++ avaliação_db2_Eduardo_Rafael/5b.sql
@@ -0,0 +1,15 @@
+explain analyze
+select p.id, p.status,ip.produto_id, ip.quantidade, ip.preco_unitario
+from pedidos p
+join itens_pedido ip
+    on ip.pedido_id = p.id
+where p.id = 100;
+create index idx_itens_pedido_pedido_id
+on itens_pedido (pedido_id);
+analyze itens_pedido;
+explain analyze
+select p.id, p.status, ip.produto_id,ip.quantidade, ip.preco_unitario
+from pedidos p
+join itens_pedido ip
+    on ip.pedido_id = p.id
+where p.id = 100;
